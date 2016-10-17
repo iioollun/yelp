1d69f45be4331a3caa2f0ff536156b9e0c2fc130
Chau Vo <minhchau273@gmail.com>
2016-10-18 00:02:39 +0700

    Create YelpClient and Business model

---
+++ Yelp/API/YelpClient.swift
@@ -0,0 +1,9 @@
+//
+//  YelpClient.swift
+//  Yelp
+//
+//  Created by Chau Vo on 10/17/16.
+//  Copyright © 2016 CoderSchool. All rights reserved.
+//
+
+import Foundation
---
+++ Yelp/Models/Business.swift
@@ -0,0 +1,9 @@
+//
+//  Business.swift
+//  Yelp
+//
+//  Created by Chau Vo on 10/17/16.
+//  Copyright © 2016 CoderSchool. All rights reserved.
+//
+
+import Foundation
